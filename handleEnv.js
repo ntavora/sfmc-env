f49184505b11c656dcaf76280094d82cc1bb0388
ntavora <ntavora@devsutd.com>
2022-09-01 18:23:28 -0300

    Add the dataextension on the project folder

--- handleEnv.js
+++ handleEnv.js
@@ -1,68 +1,107 @@
 <script runat="server" language="JavaScript">
   Platform.Load("core", "1.1.1");
   try{
-    function Enviroment(enviroment,proyectName) { 
+    function Enviroment(enviroment,proyectName) {
       // init
-      var envVars = _init(enviroment);
+      var envVars = _init(enviroment,proyectName);
       var service = {
         env: envVars
       };
-            
       return service;
       function _init(enviroment,proyectName) {
-        var env_vars = {};
-        var deName= proyectName+"_Enviroment"
-        var filtro = {
+        var env_vars = {
+        };
+        var deName   = proyectName+"_Enviroment"
+        var filtro   = {
           Property: "Name",
           SimpleOperator: "equals",
           Value: deName 
         }
         deEnv = DataExtension.Retrieve(filtro);
         if(deEnv.length == 0){
+          var folder = getFolderByName(proyectName);
+          if(folder.length == 0){
+            folder = createFolder(proyectName);
+          }
           DataExtension.Add({
             "CustomerKey": Platform.Function.GUID(),
             "Name": deName,
+            "CategoryID":folder[0].ID,
             "Fields": [
-                {
+              {
                 "Name": "Env",
                 "FieldType": "Text",
                 "MaxLength": 100,
                 "IsRequired": false
-                }
-                ,{
-                    "Name": "Key",
-                    "FieldType": "Text",
-                    "MaxLength": 200,
-                    "IsRequired": false
-                }
-                ,{
-                    "Name": "Value",
-                    "FieldType": "Text",
-                    "MaxLength": 4000,
-                    "IsRequired": false
-                }
+              }
+              ,{
+                "Name": "Key",
+                "FieldType": "Text",
+                "MaxLength": 200,
+                "IsRequired": false
+              }
+              ,{
+                "Name": "Value",
+                "FieldType": "Text",
+                "MaxLength": 4000,
+                "IsRequired": false
+              }
             ]
-          });
+          }
+                           );
         }
-        var prox = new Script.Util.WSProxy();         
+        var prox = new Script.Util.WSProxy();
         var data = prox.retrieve("DataExtensionObject[" +deName + "]",["Key", "Value"],{
           Property: "Env",
           SimpleOperator: "equals",
           Value: enviroment
-        });
+        }
+                                );
         var results =  data.Results;
         for(var i = 0; i< results.length; i++){
           var properties = results[i].Properties;
-          var key = properties[0].Value;
-          var value = properties[1].Value;
-          env_vars[key]=value;
-        }        
+          var key        = properties[0].Value;
+          var value      = properties[1].Value;
+          env_vars[key]  = value;
+        }
         return env_vars;
       }
-     
+      function getFolderByName(proyectName){
+        var complexFilter = {
+          LeftOperand: {
+            Property: "Name",
+            SimpleOperator: 'equals',
+            Value: proyectName
+          }
+          ,
+          LogicalOperator: "AND",
+          RightOperand: {
+            Property: "ContentType",
+            SimpleOperator: 'equals',
+            Value: 'dataextension'
+          }
+        };
+        return Folder.Retrieve(complexFilter)
+      }
+      function createFolder(proyectName){
+        var parentFolder = getFolderByName("Data Extensions");   
+        Write(parentFolder[0].ID)
+       var newFolder = {
+        "Name" : proyectName,
+        "CustomerKey" : proyectName +"_key",
+        "Description" : proyectName,
+        "ContentType" : "dataextension",
+        "IsActive" : "true",
+        "IsEditable" : "true",
+        "AllowChildren" : "true",
+        "ParentFolderID" : parentFolder[0].ID
+    };
+        Folder.Add(newFolder);
+        return getFolderByName(proyectName);
+      }
     }
-  //  var process = Enviroment('Prod');
-   // Write(process.env.campaing);
+   // var process = Enviroment('Prod','Prueba1');
+    // Write(process.env.campaing);
   }
   catch(ex){
     Write(Stringify(ex));
