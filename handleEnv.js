902773019a3e4fe162346ac0be65d89eb403e20b
Natalia <ntavora@devsutd.com>
2024-05-10 15:08:01 -0300

    rollback

--- handleEnv.js
+++ handleEnv.js
@@ -1,4 +1,5 @@
-
+<script runat="server" language="JavaScript">
+  Platform.Load("core", "1.1.1");
   try{
     function Enviroment(enviroment,proyectName) {
       // init
@@ -105,3 +106,4 @@
   catch(ex){
     Write(Stringify(ex));
   }
+</script>
